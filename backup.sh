9b61a2f662c1378f521294b2294993c660c0b76c
cccaternberg <acaternberg@cloudbees.com>
2025-12-08 11:05:53 +0100

    add

--- backup.sh
+++ backup.sh
@@ -1,18 +1,23 @@
 #! /bin/bash
 
-CI_POD=${1:-your_cjoc_or_CI_POD-0}
-S3BUCKET=${2:-your_s3_bucket_name}
-S3BUCKET_FOLDER="${3:-your_s3_bucket_folder}"
+export AWS_ACCESS_KEY_ID="YOUR_ACCESS_KEY"
+export AWS_SECRET_ACCESS_KEY="YOUR_SECRET_ACCESS_KEY"
+
+CI_POD=${1:-"POD_NAME"} # CJOC or Controller POD name
+S3BUCKET=${2:-"YOUR_S3_BUCKET"}
+S3BUCKET_FOLDER="${3:-"YOUR_S3_BACKUP_FOLDER"}"
 echo "create Archive for Controller: $CI_POD"
 ARCHIVENAME=backup-$CI_POD-$(date +"%d-%m-%Y").tar.gz
 
-kubectl exec -it $CI_POD -- bash -c "cd /var/jenkins_home &&  tar -cvzf   /tmp/$ARCHIVENAME ."
+# Example for entire jenkins_home backup
+#kubectl exec -it $CI_POD -- bash -c "cd /var/jenkins_home &&  tar -cvzf   /tmp/$ARCHIVENAME ."
+
+# Example for jenkins_home backup, excluding unwanted files or folders
+kubectl exec -it $CI_POD -- bash -c "cd /var/jenkins_home &&  tar -cvzf  --exclude='logs' --exclude='jobs'   /tmp/$ARCHIVENAME ."
+
 kubectl cp --retries 10  $CI_POD:/tmp/$ARCHIVENAME ./$ARCHIVENAME
 du -m ./$ARCHIVENAME
 
-#upload to gcp bucket
-#gsutil cp $ARCHIVENAME gs://$GBUCKET/backups/$ARCHIVENAME
-
 #Upload to s3
-#aws s3 cp  ./$ARCHIVENAME s3://$S3BUCKET/controllers/$ARCHIVENAME
+aws s3 cp  ./$ARCHIVENAME s3://$S3BUCKET/$S3BUCKET_FOLDER/$ARCHIVENAME
 echo $ARCHIVENAME
